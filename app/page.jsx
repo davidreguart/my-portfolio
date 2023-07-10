f111c8326af6386e0af1660aac6db8b3bc66090a
David Reguart <reguartdavid@gmail.com>
2023-07-10 22:55:03 +0200

    Starting My Portfolio React + Next Js + Tailwind

---
+++ app/page.jsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+  return (
+    <div className='h-[9000px] w-full bg-gradient-45 from-pink from-1% via-purple via-10% to-black to-40%'>
+      <h1 className='text-white h-200 w-full text-center'>This is my portfolio</h1>
+    </div>
+  )
+}
