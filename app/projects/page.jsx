3813448998617cf4b5455ee95e00fc4dded1f906
David Reguart <reguartdavid@gmail.com>
2023-07-30 15:17:30 +0200

    fix: images links

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -10,7 +10,7 @@ function Projects() {
         {/* Project 1 content */}
         <section className='relative bg-gradient-to-r from-yellow from-1% to-green to-80% md:w-96 rounded-2xl md:p-4 md:pt-8 shadow-2xl'>
           <div className='md:w-11/12 md:h-2/3 md:mx-auto rounded-2xl shadow-2xl'>
-            <Image className='object-cover w-full h-full object-top rounded-2xl' width={500} height={500} alt='Project1 thumbnail' src='/../public/img/project2_thumbnail.png' />
+            <Image className='object-cover w-full h-full object-top rounded-2xl' width={500} height={500} alt='Project1 thumbnail' src='/../public/project2_thumbnail.png' />
           </div>
           <footer className='md:w-10/12 md:mx-auto md:mt-4'>
             <h2 className='md:font-bold md:text-3xl md:mb-2'>GeniusHome</h2>
@@ -20,13 +20,13 @@ function Projects() {
           <div className='absolute inset-0 bg-lightPurple opacity-0 hover:opacity-90 rounded-2xl transition-all ease-in-out duration-300'>
             <div className='w-full h-1/2 pt-14'>
               <Link href='https://github.com/davidypaloma/GeniusHome'>
-                <Image className='w-16 mb-8 shadow-xl mx-auto' width={100} height={100} alt='Github icon' src='/../public/img/github_icon.png' />
+                <Image className='w-16 mb-8 shadow-xl mx-auto' width={100} height={100} alt='Github icon' src='/../public/github_icon.png' />
                 <p className='bg-white w-9/12 px-4 py-1 text-center mx-auto rounded-lg shadow-xl'><span>{'{'}</span> Visit this project at GitHub <span>{'}'}</span></p>
               </Link>
             </div>
             <div className='w-full h-1/2 pt-10'>
               <Link href='https://genius-home.fly.dev/'>
-                <Image className='w-16 mb-8 shadow-xl mx-auto' width={100} height={100} alt='Github icon' src='/../public/img/link_icon.png' />
+                <Image className='w-16 mb-8 shadow-xl mx-auto' width={100} height={100} alt='Github icon' src='/../public/link_icon.png' />
                 <p className='bg-white w-9/12 px-4 py-1 text-center mx-auto rounded-lg shadow-xl'><span>{'{'}</span> Visit the complet web Project <span>{'}'}</span></p>
               </Link>
             </div>
@@ -36,7 +36,7 @@ function Projects() {
         {/* Project 2 content */}
         <section className='relative bg-gradient-to-r from-yellow from-1% to-green to-80% md:w-96 rounded-2xl md:p-4 md:pt-8 shadow-2xl'>
           <div className='md:w-11/12 md:h-2/3 md:mx-auto rounded-2xl shadow-2xl'>
-            <Image className='object-cover w-full h-full object-top rounded-2xl' width={500} height={500} alt='Project2 thumbnail' src='/../public/img/project1_thumbnail.png' />
+            <Image className='object-cover w-full h-full object-top rounded-2xl' width={500} height={500} alt='Project2 thumbnail' src='/../public/project1_thumbnail.png' />
           </div>
           <footer className='md:w-10/12 md:mx-auto md:mt-4'>
             <h2 className='md:font-bold md:text-3xl md:mb-2'>Adopt Me!</h2>
@@ -46,7 +46,7 @@ function Projects() {
           <div className='absolute inset-0 bg-lightPurple opacity-0 hover:opacity-90 rounded-2xl transition-all ease-in-out duration-300'>
             <div className='w-full h-1/2 pt-14'>
               <Link href='https://github.com/davidypaloma/adopt-me-app'>
-                <Image className='w-16 mb-8 shadow-xl mx-auto' width={100} height={100} alt='Github icon' src='/../public/img/github_icon.png' />
+                <Image className='w-16 mb-8 shadow-xl mx-auto' width={100} height={100} alt='Github icon' src='/../public/github_icon.png' />
                 <p className='bg-white w-9/12 px-4 py-1 text-center mx-auto rounded-lg shadow-xl'><span>{'{'}</span> Visit this project at GitHub <span>{'}'}</span></p>
               </Link>
             </div>
--- app/technologies/page.jsx
+++ app/technologies/page.jsx
@@ -7,18 +7,18 @@ function Technologies() {
       <div className='md:w-8/12 md:h-[450px] md:gap-16 text-darkPurple'>
 
         <div className='md:flex md:justify-around md:gap-16'>
-          <SkillButton icon='/../public/img/html_logo.png' />
-          <SkillButton icon='/../public/img/css_logo.png' />
-          <SkillButton icon='/../public/img/js_logo.png' />
-          <SkillButton icon='/../public/img/react_logo.png' />
-          <SkillButton icon='/../public/img/figma_logo.png' />
+          <SkillButton icon='/../public/html_logo.png' />
+          <SkillButton icon='/../public/css_logo.png' />
+          <SkillButton icon='/../public/js_logo.png' />
+          <SkillButton icon='/../public/react_logo.png' />
+          <SkillButton icon='/../public/figma_logo.png' />
         </div>
 
         <div className='md:flex md:justify-around md:px-10 md:gap-16 md:mt-16'>
-          <SkillButton icon='/../public/img/bootstrap_logo.png' />
-          <SkillButton icon='/../public/img/tailwind_logo.png' />
-          <SkillButton icon='/../public/img/mongo_logo.png' />
-          <SkillButton icon='/../public/img/node_logo.png' />
+          <SkillButton icon='/../public/bootstrap_logo.png' />
+          <SkillButton icon='/../public/tailwind_logo.png' />
+          <SkillButton icon='/../public/mongo_logo.png' />
+          <SkillButton icon='/../public/node_logo.png' />
         </div>
 
       </div>
